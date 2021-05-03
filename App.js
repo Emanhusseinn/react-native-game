465a863a45b70ed6bd0cb24947509b6439819214
Emanhusseinn <eman.hussein.jo@gmail.com>
2021-05-03 14:58:36 +0300

    'gameSetUp'

---
+++ App.js
@@ -0,0 +1,21 @@
+import React , {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View, ScrollView, FlatList} from 'react-native';
+
+
+export default function App() {
+
+
+    return (
+      <View style={styles.container}>
+        <Text> game</Text>
+  
+    </View>
+  ); 
+}
+
+const styles = StyleSheet.create({ 
+  container:{
+    padding: 50
+  }
+
+});
